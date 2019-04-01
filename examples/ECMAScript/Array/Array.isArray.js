9a237a6e2060b098359359b1fb65c3e1ce7ec794
yanceyleo <developer@yanceyleo.com>
2019-04-01 18:51:52 +0800

    feat: Finish Array.isArray()

---
+++ examples/ECMAScript/Array/Array.isArray.js
@@ -0,0 +1,5 @@
+console.log(Array.isArray([])); // true
+console.log(Array.isArray(new Array())); // true
+console.log(Array.isArray(123)); // false
+console.log(Array.isArray({})); // false
+console.log(Array.isArray()); // false
