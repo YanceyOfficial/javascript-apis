ec7358389a85f474fdc5b2a79575ffc0cacfa584
YanceyOfficial <yanceyofficial@gmail.com>
2020-04-08 19:11:13 +0800

    feat: update docs

--- .docusaurus/client-modules.js
+++ .docusaurus/client-modules.js
@@ -1,4 +1,5 @@
 export default [
+  require("/Users/yancey/code/javascript-apis/node_modules/@docusaurus/plugin-google-analytics/src/analytics"),
   require("infima/dist/css/default/default.css"),
   require("remark-admonitions/styles/infima.css"),
   require("/Users/yancey/code/javascript-apis/src/css/custom.css"),
