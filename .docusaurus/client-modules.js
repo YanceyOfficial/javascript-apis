1539fb25d1691b232a1f60834828677969947e6f
Yancey Leo <yanceyofficial@gmail.com>
2020-04-09 12:35:55 +0800

    feat: update .gitignore

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("infima/dist/css/default/default.css"),
-  require("remark-admonitions/styles/infima.css"),
-  require("/Users/yanceyleo/code/project/javascript-apis/src/css/custom.css"),
-  require("/Users/yanceyleo/code/project/javascript-apis/node_modules/@docusaurus/theme-classic/src/prism-include-languages"),
-];
