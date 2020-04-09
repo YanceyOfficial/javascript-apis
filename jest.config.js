e4a0d5d24010f504bf34205e2b703e59c9063697
Yancey Leo <yanceyofficial@gmail.com>
2020-04-09 12:03:19 +0800

    chore: update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -23,7 +23,7 @@ module.exports = {
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
   'collectCoverageFrom': [
-    'playground/**/*.{js,jsx,ts,tsx}',
+    'playground/**/*.js',
     '!**/node_modules/**',
   ],
 
