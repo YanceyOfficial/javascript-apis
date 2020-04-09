3c1225a3b99c8ff3f0108dc75034f276f72b5b47
Yancey Leo <yanceyofficial@gmail.com>
2020-04-09 11:57:53 +0800

    chore: update .gitignore

--- .docusaurus/client-modules.js
+++ .docusaurus/client-modules.js
@@ -1,4 +1,5 @@
 export default [
+  require("/Users/yanceyleo/code/project/javascript-apis/node_modules/@docusaurus/plugin-google-analytics/src/analytics"),
   require("infima/dist/css/default/default.css"),
   require("remark-admonitions/styles/infima.css"),
   require("/Users/yanceyleo/code/project/javascript-apis/src/css/custom.css"),
