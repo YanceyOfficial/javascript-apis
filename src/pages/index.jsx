c0b8219b4e5b76df9d44b48d372342886c4b736e
YanceyOfficial <yanceyofficial@gmail.com>
2021-07-28 11:26:52 +0800

    feat: update dependencies

---
+++ src/pages/index.jsx
@@ -0,0 +1,7 @@
+// eslint-disable-next-line no-use-before-define
+import React from 'react'
+import { Redirect } from '@docusaurus/router'
+
+const Home = () => <Redirect to='/Object/hasOwnProperty' />
+
+export default Home
